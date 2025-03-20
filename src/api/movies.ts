10433e838f1fa000e160e53cccdd40a410ba4263
Maxime Moreillon <m.moreillon@gmail.com>
2025-03-20 13:32:41 +0900

    db.ts in lib

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,33 +1,33 @@
-"use server"
+"use server";
 
-import { eq } from "drizzle-orm"
-import { db } from "./db"
-import { Movies } from "~/../drizzle/schema"
+import { eq } from "drizzle-orm";
+import { db } from "../lib/db";
+import { Movies } from "~/../drizzle/schema";
 export type Movie = {
-  title: string
-}
+  title: string;
+};
 
 export async function createMovie({ title }: Movie) {
-  const [newMovie] = await db.insert(Movies).values({ title }).returning()
-  return newMovie
+  const [newMovie] = await db.insert(Movies).values({ title }).returning();
+  return newMovie;
 }
 
 export async function readMovies() {
-  return await db.select().from(Movies)
+  return await db.select().from(Movies);
 }
 
 export async function readMovie(id: number | string) {
   const [movie] = await db
     .select()
     .from(Movies)
-    .where(eq(Movies.id, Number(id)))
-  return movie
+    .where(eq(Movies.id, Number(id)));
+  return movie;
 }
 
 export async function deleteMovie(id: number | string) {
   const [deletedMovie] = await db
     .delete(Movies)
     .where(eq(Movies.id, Number(id)))
-    .returning()
-  return deletedMovie
+    .returning();
+  return deletedMovie;
 }
